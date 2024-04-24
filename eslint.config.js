fd26e5af02f1b0700c710d396fb67b88e48ea4f3
Andre Loreth <andre@loreth.co>
2024-04-24 14:17:01 +0200

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+/** @type {import("@types/eslint").Linter.FlatConfig[]} */
+module.exports = [
+	...require("@abinnovision/eslint-config-base"),
+	...require("@abinnovision/eslint-config-typescript"),
+	{files: ["**/*.js"], languageOptions: {globals: require("globals").node}},
+];
