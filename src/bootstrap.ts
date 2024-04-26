fa5be626733ef77faf9099c7edf65dccef6efeb9
Andre Loreth <andre@loreth.co>
2024-04-26 23:54:49 +0200

    fix: always use json as response

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,7 +4,25 @@ import { dispatchHandlerController } from "./dispatch-handler/controller";
 import { getConfig } from "./utils/config";
 import { getLogger } from "./utils/logger";
 
-export const bootstrap = (async () => {
+/**
+ * Describes the result of the bootstrap function.
+ * This is mostly used for testing purposes.
+ */
+interface BootstrapResult {
+	/**
+	 * Running server instance.
+	 */
+	server: Server;
+	/**
+	 * Function to shut down the current server instance.
+	 */
+	shutdown: () => void;
+}
+
+// Main entrypoint to the application, where all the startup logic is defined.
+// This function is immediately invoked when the file is imported.
+// For testing purposes, the function is exported and provides some helpers.
+export const bootstrap = (async (): Promise<BootstrapResult> => {
 	// Load the configuration first to make sure it's valid.
 	const config = getConfig();
 	const logger = getLogger("bootstrap");
@@ -14,8 +32,11 @@ export const bootstrap = (async () => {
 	// Register the handler controller.
 	server.post("/dispatch", dispatchHandlerController);
 
-	server.set_not_found_handler((req, res) => {
-		res.status(404).send("Not found");
+	// Set up a global not found handler
+	server.set_not_found_handler((_, res) => {
+		res.status(404).json({
+			error: "Not found",
+		});
 	});
 
 	await server.listen(config.PORT);
