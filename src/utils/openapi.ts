90ba115d460c172025d34e6ff692b5bf04c82521
Andre Loreth <andre@loreth.co>
2024-04-28 12:56:07 +0200

    fix: import openapi.yaml instead of loading it through fs

--- src/utils/openapi.ts
+++ src/utils/openapi.ts
@@ -1,23 +1,9 @@
-import fsp from "fs/promises";
-import path from "path";
-
-let _cache: string | null = null;
+// @ts-ignore
+import content from "../assets/openapi/openapi.yaml";
 
 /**
  * Provides the OpenAPI specification as a string in YAML format.
  */
 export async function getOpenApiSpec(): Promise<string> {
-	if (!_cache) {
-		const specPath = path.join(__dirname, "../assets/openapi/openapi.yaml");
-
-		try {
-			_cache = await fsp.readFile(specPath, "utf-8");
-		} catch (err) {
-			console.error("Failed to read OpenAPI spec file", err);
-
-			throw err;
-		}
-	}
-
-	return _cache;
+	return content;
 }
