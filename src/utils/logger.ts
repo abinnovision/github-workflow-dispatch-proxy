2d2fec5015cd76cf2a6b1687c6577d4d54874301
Andre Loreth <andre@loreth.co>
2024-05-06 22:24:39 +0200

    fix: redact secrets

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,6 +4,7 @@ import type { Logger } from "pino";
 
 const _logger = pino({
 	level: process.env.APP_LOG_LEVEL || "info",
+	redact: ["config.GH_AUTH_APP_PRIVATE_KEY", "config.GH_AUTH_TOKEN"],
 });
 
 /**
