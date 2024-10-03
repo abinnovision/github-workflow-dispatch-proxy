aec54fabdd9e62d0b0f751140cf8b72dbcb9201e
André L. <andre@loreth.co>
2024-10-03 13:30:26 +0200

    fix: use pino-http for logging within controller (#22)

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,15 @@
 import { pino } from "pino";
 
+import type { Request } from "express";
 import type { Logger } from "pino";
 
 const _logger = pino({
 	level: process.env.APP_LOG_LEVEL || "info",
-	redact: ["config.GH_AUTH_APP_PRIVATE_KEY", "config.GH_AUTH_TOKEN"],
+	redact: [
+		"config.GH_AUTH_APP_PRIVATE_KEY",
+		"config.GH_AUTH_TOKEN",
+		"req.headers.authorization",
+	],
 });
 
 /**
@@ -18,4 +23,18 @@ export const getLogger = (module?: string): Logger => {
 	}
 
 	return _logger.child({ module });
+};
+
+/**
+ * Provides the logger for the given request.
+ *
+ * @param req The request to get the logger for.
+ */
+export const getLoggerForRequest = (req: Request): Logger => {
+	const logInterface = (req as any).log as pino.Logger;
+	if (!logInterface) {
+		throw new Error("Request does not have a logger");
+	}
+
+	return logInterface;
 };
