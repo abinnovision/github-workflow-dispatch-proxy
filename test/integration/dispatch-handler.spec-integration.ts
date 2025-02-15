4f9b66eedb8063b3666c589b570d7914af8520c8
André L. <andre@loreth.co>
2025-02-15 13:48:37 +0100

    feat: set "ref" as optional and fetch the default branch if necessary (#91)

--- test/integration/dispatch-handler.spec-integration.ts
+++ test/integration/dispatch-handler.spec-integration.ts
@@ -1,7 +1,8 @@
+/* eslint-disable max-nested-callbacks */
 import { getPort } from "get-port-please";
 import nock from "nock";
 import request from "supertest";
-import { afterEach, beforeEach, describe, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { setupOAuthServer } from "../utils/setup-oauth-server.js";
 import { testDataIdTokenClaims } from "../utils/test-data.js";
@@ -67,6 +68,83 @@ describe("dispatch-handler", () => {
 					},
 				})
 				.expect(200);
+		});
+
+		it("should resolve the default branch if no ref is given", async () => {
+			const _testDefaultBranch = "main-default";
+
+			const idToken = await oAuthServer.buildToken(testDataIdTokenClaims);
+
+			const dispatchScope = nock("https://api.github.com")
+				.post(
+					"/repos/octo-org/octo-repo/actions/workflows/example-workflow/dispatches"
+				)
+				.reply((uri, body) => {
+					if ((body as any).ref !== _testDefaultBranch) {
+						return [400];
+					}
+
+					return [200];
+				});
+
+			const getScope = nock("https://api.github.com")
+				.get("/repos/octo-org/octo-repo")
+				.reply(200, () => ({
+					default_branch: _testDefaultBranch,
+				}));
+
+			await st
+				.post("/dispatch")
+				.auth(idToken, { type: "bearer" })
+				.send({
+					target: {
+						owner: "octo-org",
+						repo: "octo-repo",
+						workflow: "example-workflow",
+					},
+					inputs: {
+						type: "production",
+					},
+				})
+				.expect(200);
+
+			dispatchScope.done();
+			getScope.done();
+		});
+
+		it("should not load default branch if ref is given", async () => {
+			const idToken = await oAuthServer.buildToken(testDataIdTokenClaims);
+
+			const dispatchScope = nock("https://api.github.com")
+				.post(
+					"/repos/octo-org/octo-repo/actions/workflows/example-workflow/dispatches"
+				)
+				.reply(200);
+
+			const getScope = nock("https://api.github.com")
+				.get("/repos/octo-org/octo-repo")
+				.reply(200, () => ({ default_branch: "main" }));
+
+			await st
+				.post("/dispatch")
+				.auth(idToken, { type: "bearer" })
+				.send({
+					target: {
+						owner: "octo-org",
+						repo: "octo-repo",
+						workflow: "example-workflow",
+						ref: "master",
+					},
+					inputs: {
+						type: "production",
+					},
+				})
+				.expect(200);
+
+			dispatchScope.done();
+
+			// Expect that the request to load the default branch was not made.
+			expect(getScope.isDone()).toBe(false);
 		});
 	});
 });
